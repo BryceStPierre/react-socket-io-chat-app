eaec923d2ad9848da18a3f79bc8ac38f53affe60
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-02-21 13:06:34 -0500

    Added Sidebar with icons.

--- src/chats/ChatContainer.js
+++ src/chats/ChatContainer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import Sidebar from './Sidebar';
+
 export default class ChatContainer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chats: [],
+            activeChat: null
+        };
+    }
+
+    setActiveChat = (activeChat) => {
+        this.setState({ activeChat });
+    }
+
     render () {
         const { user, logout } = this.props;
+        const { chats, activeChat } = this.state;
         return (
-            <div>Chat Container</div>
+            <div className="container">
+                <Sidebar
+                    logout={logout}
+                    chats={chats}
+                    user={user}
+                    activeChat={activeChat}
+                    setActiveChat={this.setActiveChat}
+                    />
+            </div>
         );
     }
 }
